acc12c4fade95541ad5e067d290e5a125063202d
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 12:03:49 -0700

    Added Add,Subtract, Multiply, Divide functions

---
+++ arithmetic.py
@@ -0,0 +1,38 @@
+"""Math functions for calculator."""
+
+
+def add(num1, num2):
+    """Return the sum of the two inputs."""
+
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    """Return the second number subtracted from the first."""
+
+    return num1 - num2
+    
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+
+    return num1 * num2
+
+def divide(num1, num2):
+    """Divide the first input by the second and return the result."""
+
+    return num1/num2
+
+def square(num1):
+    """Return the square of the input."""
+
+
+def cube(num1):
+    """Return the cube of the input."""
+
+
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
+
+
+def mod(num1, num2):
+    """Return the remainder of num1 / num2."""
