ad01a87d9bcc49e833a443b75394beb327cf4b29
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 12:10:15 -0700

    Added square, cube, power and modulo functionality

--- arithmetic.py
+++ arithmetic.py
@@ -11,7 +11,7 @@ def subtract(num1, num2):
     """Return the second number subtracted from the first."""
 
     return num1 - num2
-    
+
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
@@ -25,14 +25,19 @@ def divide(num1, num2):
 def square(num1):
     """Return the square of the input."""
 
+    return num1 * num1
 
 def cube(num1):
     """Return the cube of the input."""
-
+    
+    return num1**3
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    return num1 ** num2
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+
+    return num1 % num2
